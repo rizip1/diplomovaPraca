c23c81ed2371ff225b0a1a01dede7112a7f90f63
Richard Izip <richardizip@gmail.com>
2017-10-16 22:54:17 +0200

    Rework GRU to random sampling

--- gru.py
+++ gru.py
@@ -1,15 +1,16 @@
 import pandas as pd
 import numpy as np
+import sklearn.linear_model as lm
+from keras.constraints import maxnorm
 
-from random import randint
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 from keras import backend
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Dropout
 from keras.layers import GRU
-from keras.optimizers import SGD
+from random import randint
 
 from feature_utils import add_min_max
 from feature_utils import add_moments
@@ -48,104 +49,114 @@ dropped_data = dataset.drop(to_drop, axis=1)
 x = dropped_data.drop('future_temp', axis=1)
 y = dropped_data.future_temp
 
-random_s = 300
-
 data_len = dataset.shape[0]
-
-window_size = 24 * 30
-offset = 10679 - window_size
-train_end = window_size + offset
-
-if (random_s == 0):
-    data_len = train_end + 30
+window_len = 24 * 120
 
 all_predicted = np.array([])
 all_y_test = np.array([])
 
-first_predictions = np.array([])
-first_y_test = np.array([])
-
-first_30_predictions = np.array([])
-first_30_y_test = np.array([])
-
-step = 24
+interval = 24
+start = window_len
 predictions_made = 0
 mae_predict = 0
 mse_predict = 0
 
-while ((random_s == 0 and train_end < data_len) or
-       (random_s != 0 and predictions_made < random_s)):
-    pred_length = step
-    if (pred_length + train_end > data_len):
-        pred_length = data_len - train_end
+total_predictions = 1000
 
-    if (random_s == 0):
-        print('\nCurrent position: ...', train_end)
+linear_mae = 0
+linear_mse = 0
+neural_mae = 0
+neural_mse = 0
 
-    for i in range(pred_length):
-        if (random_s != 0):
-            train_end = randint(window_size, data_len - i - 25)
-            print('position', train_end)
+reg_better = 0
+nn_better = 0
 
-        train_X = x.iloc[train_end - window_size +
-                         i: train_end: step, :].values
-        train_y = y.iloc[train_end - window_size +
-                         i: train_end: step].values.reshape(-1, 1)
+for j in range(total_predictions):
+    i = randint(start, data_len - 1)
 
-        test_X = x.iloc[train_end + i: train_end + pred_length: step, :].values
-        test_y = y.iloc[train_end + i: train_end +
-                        pred_length: step].values.reshape(-1, 1)
+    train_X = x.iloc[i - start:i:interval, :].values
+    train_y = y.iloc[i - start:i:interval].values.reshape(-1, 1)
 
-        scaler = MinMaxScaler(feature_range=(0, 1))
-        scaled = scaler.fit_transform(
-            np.concatenate((train_X, train_y), axis=1))
+    test_X = x.iloc[i, :].values.reshape(1, -1)
+    test_y = y.iloc[i].reshape(-1, 1)
 
-        train_X = scaled[:, :-1]
-        train_y = scaled[:, -1]
+    train_X_orig = train_X.copy()
+    train_y_orig = train_y.copy()
+    test_X_orig = test_X.copy()
+    test_y_orig = test_y.copy()
 
-        tmp = scaler.transform(np.concatenate((test_X, test_y), axis=1))
-        test_X = tmp[:, :-1]
-        test_y = tmp[:, -1]
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaled = scaler.fit_transform(
+        np.concatenate((train_X, train_y), axis=1))
 
-        train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
-        test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
+    train_X = scaled[:, :-1]
+    train_y = scaled[:, -1]
 
-        # print('training network {}'.format(i))
-        # return_sequences=True (use for more layers)
-        model = Sequential()
-        model.add(GRU(50, input_shape=(train_X.shape[1], train_X.shape[2]),
-                      activation='tanh'))
-        model.add(Dense(1))
-        sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-        model.compile(loss='mae', optimizer='sgd')
-        history = model.fit(train_X, train_y, epochs=100, batch_size=4,
-                            verbose=0, shuffle=False)
-        yhat1 = model.predict(test_X)
+    tmp = scaler.transform(np.concatenate((test_X, test_y), axis=1))
+    test_X = tmp[:, :-1]
+    test_y = tmp[:, -1]
 
-        # free tensorFlow memory
-        backend.clear_session()
+    train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
+    test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
 
-        test_X = test_X.reshape((test_X.shape[0], test_X.shape[2]))
+    model = Sequential()
+    model.add(GRU(300, input_shape=(train_X.shape[1], train_X.shape[2]),
+                  activation='tanh', kernel_constraint=maxnorm(3)))
+    model.add(Dropout(0.3))
+    model.add(Dense(1))
+    model.compile(loss='mse', optimizer='RMSprop')
+    history = model.fit(train_X, train_y, epochs=100, batch_size=10,
+                        verbose=0, shuffle=False)
+    yhat = model.predict(test_X)
 
-        # invert scaling for forecast
-        y_predicted = get_predicted_values(test_X, yhat1)
+    # free tensorFlow memory
+    backend.clear_session()
 
-        # invert scaling for actual
-        y_test = get_test_values(test_y, test_X)
+    test_X = test_X.reshape((test_X.shape[0], test_X.shape[2]))
 
-        predictions_made += len(y_test)
-        mae_predict += np.sum(abs(y_test - y_predicted))
-        mse_predict += np.sum((y_test - y_predicted) ** 2)
+    linear_model = lm.LinearRegression(fit_intercept=True)
+    linear_model.fit(train_X_orig, train_y_orig)
+    linear_prediction = linear_model.predict(test_X_orig)
 
-        print('\nmse', mse_predict / predictions_made)
-        print('mae', mae_predict / predictions_made)
-        print('predictions count', predictions_made)
+    # invert scaling for forecast
+    y_predicted = get_predicted_values(test_X, yhat)
 
-        all_y_test = np.append(all_y_test, y_test)
-        all_predicted = np.append(all_predicted, y_predicted)
+    # invert scaling for actual
+    y_test = get_test_values(test_y, test_X)
 
-    # shift interval for learning
-    train_end += pred_length
+    predictions_made += len(y_test)
+
+    neural_mae += np.sum(abs(y_test - y_predicted))
+    neural_mse += np.sum((y_test - y_predicted) ** 2)
+
+    linear_mae += np.sum(abs(y_test - linear_prediction))
+    linear_mse += np.sum((y_test - linear_prediction) ** 2)
+
+    if (abs(y_test - linear_prediction) < abs(y_test - y_predicted)):
+        reg_better += 1
+
+    if (abs(y_test - linear_prediction) > abs(y_test - y_predicted)):
+        nn_better += 1
+
+    print('\nmae-lin-neu', linear_mae / predictions_made,
+          neural_mae / predictions_made)
+    print('mse-lin-neu', linear_mse / predictions_made,
+          neural_mse / predictions_made)
+
+    y_predicted = (y_predicted + linear_prediction) / 2
+
+    mae_predict += np.sum(abs(y_test - y_predicted))
+    mse_predict += np.sum((y_test - y_predicted) ** 2)
+
+    print('mae', mae_predict / predictions_made)
+    print('mse', mse_predict / predictions_made)
+    print('predictions count', predictions_made)
+
+    print('\nreg_better', reg_better)
+    print('nn_better', nn_better)
+
+    all_y_test = np.append(all_y_test, y_test)
+    all_predicted = np.append(all_predicted, y_predicted)
 
 print('\nPredictions count', len(all_predicted))
 print('MAE', mean_absolute_error(all_predicted, all_y_test))
