7caf87317c1bbc328396e22f7a739882bdaed197
Richard Izip <richardizip@gmail.com>
2018-03-19 21:33:35 +0100

    Simplify stable weather detection

--- stable_weather_detection.py
+++ stable_weather_detection.py
@@ -1,5 +1,4 @@
 import numpy as np
-import re
 
 '''
 No need to handle 'out of bound index' for any reasonable window length
@@ -19,7 +18,24 @@ def is_error_diff_enough(model_errors):
     return (error_diff / max_error) < threshold
 
 
-def s1(data, pos, offset1=24, offset2=48):
+def s1(data, pos, offset=24):
+    '''
+    Distances between predicted temperatures in t+i and t-24+i must not
+    be greater than THRESHOLD for i=[-5, -4, -3, -2, -1, 0]
+    '''
+    hours = [-5, -4, -3, -2, -1, 0]
+    threshold = 1
+
+    for h in hours:
+        v1 = data.loc[pos + h, 'future_temp_shmu']
+        v2 = data.loc[pos - offset + h, 'future_temp_shmu']
+        handle_missing(v1, v2)
+        if (abs(v1 - v2) > threshold):
+            return False
+    return True
+
+
+def s2(data, pos, offset1=24, offset2=48):
     '''
     Distances between temperatures in t-24+i and t-48+i must not be greater
     than THRESHOLD for i=[-2,-1,0,1,2]
@@ -36,7 +52,7 @@ def s1(data, pos, offset1=24, offset2=48):
     return True
 
 
-def s2(data, pos, offset1=24, offset2=48):
+def s3(data, pos, offset1=24, offset2=48):
     '''
     Gaussian weighted distances between temperatures in t-24+i and t-48+i
     must not be greater than THRESHOLD
@@ -59,99 +75,10 @@ def s2(data, pos, offset1=24, offset2=48):
     return True
 
 
-def s3(data, pos, offset1=0, offset2=24):
-    '''
-    Distances between temperatures in ref_time+i and ref_time-24+i must not
-    be greater than THRESHOLD for i=[-2,-1,0,1,2]
-    '''
-    hours = [-4, -3, -2, -1, 0]
-    threshold = 1
-
-    val_date = data.loc[pos, 'validity_date']
-    m = re.search(
-        r'^[0-9]{4}-[0-9]{2}-[0-9]{2} ([0-9]{2}):[0-9]{2}:[0-9]{2}$',
-        val_date)
-    hour = int(m.group(1))
-    offset = hour
-    if (hour > 12):
-        offset -= 12
-    if (hour == 0):
-        offset = 12
-    offset -= 1
-
-    o_pos = pos - offset
-
-    for h in hours:
-        v1 = data.loc[o_pos - offset1 + h, 'current_temp']
-        v2 = data.loc[o_pos - offset2 + h, 'current_temp']
-        handle_missing(v1, v2)
-        if (abs(v1 - v2) > threshold):
-            return False
-    return True
-
-
-def s4(data, pos):
-    '''
-    mark as stable also if weather conditions from last day
-    match well with weather conditions measured three days ago.
-    consider prediction time
-    '''
-    return s1(data, pos, offset1=24, offset2=72) or s1(data, pos)
-
-
-def s5(data, pos):
-    '''
-    mark as stable also if weather conditions from last day
-    match well with weather conditions measured three days ago.
-    consider prediction time
-    '''
-    return s2(data, pos, offset1=24, offset2=72) or s2(data, pos)
-
-
-def s6(data, pos):
-    '''
-    mark as stable also if weather conditions from time before making
-    the prediction match well with weather conditions measured
-    two days ago (from time before making the prediction)
-    '''
-    return s3(data, pos, offset1=0, offset2=48) or s3(data, pos)
-
-
-def s7(data, pos, offset=24):
-    '''
-    Distances between predicted temperatures in t+i and t-24+i must not
-    be greater than THRESHOLD for i=[-2,-1,0]
-    '''
-    hours = [-5, -4, -3, -2, -1, 0]
-    threshold = 1
-
-    for h in hours:
-        v1 = data.loc[pos + h, 'future_temp_shmu']
-        v2 = data.loc[pos - offset + h, 'future_temp_shmu']
-        handle_missing(v1, v2)
-        if (abs(v1 - v2) > threshold):
-            return False
-    return True
-
-
-def union1(data, pos):
-    return s1(data, pos) or s2(data, pos) or s3(data, pos)
-
-
-def union2(data, pos):
-    return s4(data, pos) or s5(data, pos) or s6(data, pos)
-
-
 stable_functions = {
     's1': s1,
     's2': s2,
     's3': s3,
-    's4': s4,
-    's5': s5,
-    's6': s6,
-    's7': s7,
-    'union1': union1,
-    'union2': union2,
 }
 
 
